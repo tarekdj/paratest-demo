d77d3ab7db29ce7dab61f379455ef7d50bb82059
Tarek Djebali <tarekdj@gmail.com>
2021-01-06 09:43:53 +0100

    Initial commit

---
+++ tests/FourthTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class FourthTest extends TestCase
+{
+    public function testIsFalse(): void
+    {
+        sleep(5);
+        $this->assertFalse(false);
+    }
+
+    public function testIsTrue(): void
+    {
+        sleep(5);
+        $this->assertTrue(true);
+    }
+
+    public function testIsNull(): void
+    {
+        sleep(5);
+        $this->assertNull(null);
+    }
+
+    public function testIsArray(): void
+    {
+        sleep(5);
+        $this->assertIsArray([]);
+    }
+
+}
